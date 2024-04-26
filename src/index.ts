9f6a0656a22e546b44f1b6bd3d33b59b0d7d340e
sheltonFarias <sheltonm10@outlook.com>
2024-04-26 13:24:23 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import { routes } from './routes/routes';
-
+var cors = require('cors')
 const app = express()
 
 app.use(express.json());
+
+app.use(cors({
+	origin: '*',
+	methods:'GET, PUT, POST, DELETE'
+}))
 
 app.use(routes)
 
