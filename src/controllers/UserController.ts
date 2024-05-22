dcf0b5197920d6fb728ca18a97713f08074a8dc3
SheltonFarias <sheltonm10@outlook.com>
2024-05-22 10:14:15 -0300

    alter UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -23,26 +23,22 @@ export class UserController {
   }
 
   async create(req: Request, res: Response) {
-    const { email, name, login, password, img, token } = req.body;
-    const userExists = await prisma.user.findUnique({ where: { email } })
-    const passwordHash = await bcrypt.hash(password, 10)
+    const user = req.body;
+    const userExists = await prisma.user.findUnique({ where: { email:user.email } })
+    const passwordHash = await bcrypt.hash(user.password, 10)
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    const user = await prisma.user.create({
+    const users = await prisma.user.create({
       data: {
-        email,
-        name,
-        login,
-        password: passwordHash,
-        img,
-        token
+        ...user,
+        password:passwordHash
       }
     })
-    if ((userExists) && (email.length < 5 || email.length > 100) && (!emailRegex.test(email))) {
+    if ((userExists) && (user.email.length < 5 || user.email.length > 100) && (!emailRegex.test(user.email))) {
       return res
         .status(400)
         .json({ error: 'error when creating user' })
     }
-    return res.json({ user, message: 'Create User sucess' })
+    return res.json({ users, message: 'Create User sucess' })
   }
 
   async delete(req: Request, res: Response) {
@@ -62,20 +58,16 @@ export class UserController {
 
   async update(req: Request, res: Response) {
     const userId = req.params.id;
-    const { email, name, login, password, img, token } = req.body;
-    const user = await prisma.user.findUnique({ where: { id: parseInt(userId) } });
+    const user = req.body;
+    const users = await prisma.user.findUnique({ where: { id: parseInt(userId) } });
     const updatedUser = await prisma.user.update({
       where: { id: parseInt(userId) },
       data: {
-        email,
-        name,
-        login,
-        password: password ? await bcrypt.hash(password, 10) : undefined,
-        img,
-        token
+        ...user,
+        password: user.password ? await bcrypt.hash(user.password, 10) : undefined,
       }
     });
-    if (user) {
+    if (users) {
       return res.status(200).json({ user: updatedUser });
     } else {
       return res.status(500).json({ error: 'Error updating user' });
