7c0f931dff34720d5b1c400387caeb25c4c8e08e
sheltonFarias <sheltonm10@outlook.com>
2024-08-01 10:54:52 -0300

    correction of login

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -14,11 +14,14 @@ export class AuthController {
         ]
       }
     })
-    const sha256Password = crypto.createHash('sha256').update(password).digest('hex')
-    const token = user ? jwt.sign({ id: user.id }, 'secret', { expiresIn: '1h' }) : null
-    if (!user && sha256Password !== user.password) {
+    if (!user) {
       return res.status(401).json({ message: 'invalid credentials' })
     }
+    const sha256Password = crypto.createHash('sha256').update(password).digest('hex')
+    if (sha256Password !== user.password) {
+      return res.status(401).json({ message: 'invalid credentials' })
+    }
+    const token = user ? jwt.sign({ id: user.id }, 'secret', { expiresIn: '1h' }) : null
     delete user.password
 
     await prisma.user.update({
--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,19 @@ import { routes } from '@/routes/routes'
 import path from 'path'
 
 const cors = require('cors')
-
 const app = express()
 
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
-
 app.use(express.json())
-
 app.use(cors({
 	origin: '*',
 	methods: 'GET, PUT, POST, DELETE'
 }))
 
+//routes
 app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
+// server
 app.listen(3000, () => {
 	console.log('Server on: http://localhost:3000 ON!')
 })
